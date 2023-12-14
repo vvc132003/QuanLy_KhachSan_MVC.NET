5ffa86429b6efc0617e457540cbd3e015e260c4e
vvc132003 <vvc132003@gmail.com>
2023-12-14 22:03:02 +0700

    thay đổi giao diện trang chủ giành cho khách hàng

--- QuanLyKhachSan_MVC.NET/Content/static/js/main.js
+++ QuanLyKhachSan_MVC.NET/Content/static/js/main.js
@@ -1,109 +1,68 @@
-/*  ---------------------------------------------------
-    Template Name: Sona
-    Description: Sona Hotel Html Template
-    Author: Colorlib
-    Author URI: https://colorlib.com
-    Version: 1.0
-    Created: Colorlib
----------------------------------------------------------  */
-
-'use strict';
-
 (function ($) {
-
-    /*------------------
-        Preloader
-    --------------------*/
-    $(window).on('load', function () {
-        $(".loader").fadeOut();
-        $("#preloder").delay(200).fadeOut("slow");
+    "use strict";
+    
+    // Initiate the wowjs animation library
+    new WOW().init();
+    
+    // Initiate menu
+    $('#header').after('<div class="mobile-menu d-xl-none">');
+    $('.top-menu').clone().appendTo('.mobile-menu');
+    $('.mobile-menu-btn').click(function () {
+        $('.mobile-menu').stop().slideToggle();
     });
-
-    /*------------------
-        Background Set
-    --------------------*/
-    $('.set-bg').each(function () {
-        var bg = $(this).data('setbg');
-        $(this).css('background-image', 'url(' + bg + ')');
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
     });
-
-    //Offcanvas Menu
-    $(".canvas-open").on('click', function () {
-        $(".offcanvas-menu-wrapper").addClass("show-offcanvas-menu-wrapper");
-        $(".offcanvas-menu-overlay").addClass("active");
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
     });
-
-    $(".canvas-close, .offcanvas-menu-overlay").on('click', function () {
-        $(".offcanvas-menu-wrapper").removeClass("show-offcanvas-menu-wrapper");
-        $(".offcanvas-menu-overlay").removeClass("active");
+    
+    // Date and time picker
+    $('#date-1, #date-2, #date-3, #date-4').datetimepicker({
+        format: 'L'
     });
-
-    // Search model
-    $('.search-switch').on('click', function () {
-        $('.search-model').fadeIn(400);
+    $('#time-1, #time-2').datetimepicker({
+        format: 'LT'
     });
-
-    $('.search-close-switch').on('click', function () {
-        $('.search-model').fadeOut(400, function () {
-            $('#search-input').val('');
-        });
-    });
-
-    /*------------------
-		Navigation
-	--------------------*/
-    $(".mobile-menu").slicknav({
-        prependTo: '#mobile-menu-wrap',
-        allowParentLinks: true
-    });
-
-    /*------------------
-        Hero Slider
-    --------------------*/
-   $(".hero-slider").owlCarousel({
-        loop: true,
-        margin: 0,
-        items: 1,
-        dots: true,
-        animateOut: 'fadeOut',
-        animateIn: 'fadeIn',
-        smartSpeed: 1200,
-        autoHeight: false,
+    
+    //Portfolio modal slider
+    $('.port-slider').delay(10000);
+    $('.port-slider').slick({
         autoplay: true,
-        mouseDrag: false
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.port-slider-nav'
     });
-
-    /*------------------------
-		Testimonial Slider
-    ----------------------- */
-    $(".testimonial-slider").owlCarousel({
-        items: 1,
+    $('.port-slider-nav').slick({
+        autoplay: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        asNavFor: '.port-slider',
+        arrows: false,
         dots: false,
-        autoplay: true,
-        loop: true,
-        smartSpeed: 1200,
-        nav: true,
-        navText: ["<i class='arrow_left'></i>", "<i class='arrow_right'></i>"]
+        centerMode: true,
+        focusOnSelect: true
     });
+    
+    $('#popover-content-download').hide();
+    $("[data-toggle=popover]").each(function (e) {
+        $(this).popover({
+            html: true,
+            content: function () {
+                var id = $(this).attr('id')
+                return $('#popover-content-' + id).html();
+            }
+        });
 
-    /*------------------
-        Magnific Popup
-    --------------------*/
-    $('.video-popup').magnificPopup({
-        type: 'iframe'
     });
-
-    /*------------------
-		Date Picker
-	--------------------*/
-    $(".date-input").datepicker({
-        minDate: 0,
-        dateFormat: 'dd MM, yy'
-    });
-
-    /*------------------
-		Nice Select
-	--------------------*/
-    $("select").niceSelect();
-
 })(jQuery);
+
